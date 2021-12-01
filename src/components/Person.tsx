35100387312554bbeb2ad599958bfa28aaff83da
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2021-12-01 15:40:29 +0100

    Lennart

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,1 +1,16 @@
-import <React></React>
+import React from "react";
+
+interface IPerson {
+    firstName: string;
+    secondName: string;
+}
+
+const Person = ({ firstName, secondName }: IPerson) => {
+    return (
+        <div>
+            {firstName} {secondName}
+        </div>
+    );
+};
+
+export default Person;
