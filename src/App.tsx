9f8f76508b047608b6f1808ccaa3e47cc1777bcb
LennartBecke <94056489+LennartBecke@users.noreply.github.com>
2021-12-01 16:42:03 +0100

    weiteres O

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cryptoduckk <code>src/App.tsx</code> find your cryptoooo.
+          Cryptoduckk <code>src/App.tsx</code> find your cryptooooo.
         </p>
         <a
           className="App-link"
